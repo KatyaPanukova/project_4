dd7688f3af0e1bcbb109109a7e1b503e80c2be98
KatyaPanukova <panyukowa-yekaterina@yandex.ru>
2019-11-02 21:57:51 +0300

    my project part 2

--- project_4.1.py
+++ project_4.1.py
@@ -5,7 +5,7 @@ import time
 print(lc.TXT_1)
 print(lc.TXT_2)
 
-list_1 = input()
+list_1 = input()                                 # Pen input.
 
 print(lc.TXT_3)
 print(lc.TXT_5)
@@ -14,16 +14,16 @@ print(lc.TXT_4)
 result = lc.TXT_7
 answer_2 = input(lc.TXT_6,)
 
-for garb in string.punctuation:
-    answer_2 = answer_2.replace(garb, '')
+for garb in string.punctuation:                  # Search for punctuation.
+    answer_2 = answer_2.replace(garb, '')        # Remove the punctuation mark.
 
-if answer_2 != lc.TXT_7:
+if answer_2 != lc.TXT_7:                         # If the entered data is not equal to the answer.
 
-    x = 0
+    x = 0                                        # The number of letters for clues.
 
     for num in range(4, 1, -1):
 
-        if answer_2 != lc.TXT_7:
+        if answer_2 != lc.TXT_7:                 # If the entered data is not equal to the answer.
 
             print(lc.TXT_9, num - 1)
             print(lc.TXT_17, result[x])
@@ -33,20 +33,20 @@ if answer_2 != lc.TXT_7:
 
                 answer_2 = answer_2.replace(garb, '')
 
-        elif answer_2 == lc.TXT_7:
-            ot = ' '
-            b = 1
-            a = 1
+        elif answer_2 == lc.TXT_7:               # If the entered data is not equal to the answer.
+            ot = ' '                             # Spaces for drawing.
+            b = 1                                # Number of lines.
+            a = 1                                # Number of columns.
             lis_1 = ''
-            k = 1
+            k = 1                                # Additional counter.
 
             while b != 6 and a != 6:
 
-                lis_1 += list_1[0]
+                lis_1 += list_1[0]               # Add an item to the list.
                 a += 1
                 b += 1
 
-                if b == 6 and a == 6:
+                if b == 6 and a == 6:            # If the entered data is not equal to the answer.
 
                     for k in range(1, 4):
                         print(ot * 5, lis_1)
@@ -56,11 +56,11 @@ if answer_2 != lc.TXT_7:
 
             while b != 7 and a != 7:
 
-                lis_1 += list_1[0]
+                lis_1 += list_1[0]              # Add an item to the list.
                 a += 1
                 b += 1
 
-                if b == 7 and a == 7:
+                if b == 7 and a == 7:           # If the entered data is not equal to the answer.
 
                     for k in range(1, 6):
 
@@ -76,7 +76,7 @@ if answer_2 != lc.TXT_7:
 
                 print(ot * 5, lis_1[0], ot * 2, lis_1[0])
             print()
-            for seconds in range(3):
+            for seconds in range(3):            # Cycle simulating time change in seconds.
                 if seconds == 0:
                     print(lc.TXT_8)
                 elif seconds == 1:
@@ -86,14 +86,14 @@ if answer_2 != lc.TXT_7:
                 time.sleep(1)
 
             break
-        x += 1
+        x += 1                                  # Move to the next hint letter
 
     else:
         print(lc.TXT_10)
 
         answer_3 = input()
 
-        if answer_3 == lc.TXT_11:
+        if answer_3 == lc.TXT_11:               # If the entered data is not equal to the answer.
 
             ot = ' '
             b = 1
@@ -103,11 +103,11 @@ if answer_2 != lc.TXT_7:
 
             while b != 6 and a != 6:
 
-                lis_1 += list_1[0]
+                lis_1 += list_1[0]              # Add an item to the list.
                 a += 1
                 b += 1
 
-                if b == 6 and a == 6:
+                if b == 6 and a == 6:           # If the entered data is not equal to the answer.
 
                     for k in range(1, 4):
                         print(ot * 5, lis_1)
@@ -117,11 +117,11 @@ if answer_2 != lc.TXT_7:
 
             while b != 7 and a != 7:
 
-                lis_1 += list_1[0]
+                lis_1 += list_1[0]              # Add an item to the list.
                 a += 1
                 b += 1
 
-                if b == 7 and a == 7:
+                if b == 7 and a == 7:           # If the entered data is not equal to the answer.
 
                     for k in range(1, 6):
 
@@ -141,7 +141,7 @@ if answer_2 != lc.TXT_7:
             print()
             print(lc.TXT_13)
 
-elif answer_2 == lc.TXT_7:
+elif answer_2 == lc.TXT_7:                      # If the entered data is not equal to the answer.
 
     ot = ' '
     b = 1
@@ -151,11 +151,11 @@ elif answer_2 == lc.TXT_7:
 
     while b != 6 and a != 6:
 
-        lis_1 += list_1[0]
+        lis_1 += list_1[0]                      # Add an item to the list.
         a += 1
         b += 1
 
-        if b == 6 and a == 6:
+        if b == 6 and a == 6:                   # If the entered data is not equal to the answer.
 
             for k in range(1, 4):
 
@@ -167,11 +167,11 @@ elif answer_2 == lc.TXT_7:
 
     while b != 7 and a != 7:
 
-        lis_1 += list_1[0]
+        lis_1 += list_1[0]                      # Add an item to the list.
         a += 1
         b += 1
 
-        if b == 7 and a == 7:
+        if b == 7 and a == 7:                   # If the entered data is not equal to the answer.
 
             for k in range(1, 6):
 
@@ -187,7 +187,7 @@ elif answer_2 == lc.TXT_7:
 
         print(ot * 5, lis_1[0], ot * 2, lis_1[0])
 
-    for seconds in range(3):
+    for seconds in range(3):                    # Cycle simulating time change in seconds.
         if seconds == 0:
             print(lc.TXT_8)
         elif seconds == 1:
